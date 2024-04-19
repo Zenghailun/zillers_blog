9ea62b60786f984fe0152f7684cb12953019c4a3
Zeng Hailun <zengboheng01@gmail.com>
2024-04-19 10:51:03 +0800

    feat: init cloudAlbum

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/cloudPhotoAlbum', title: '云相册' },
   { href: '/about', title: 'About' },
 ]
 
