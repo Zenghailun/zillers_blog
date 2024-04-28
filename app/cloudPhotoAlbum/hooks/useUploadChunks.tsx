65e8fe56deb9147f7f995b8cef237f1cc0048dc9
Zeng Hailun <zengboheng01@gmail.com>
2024-04-28 14:04:10 +0800

    feat: add request scheduler

--- app/cloudPhotoAlbum/hooks/useUploadChunks.tsx
+++ app/cloudPhotoAlbum/hooks/useUploadChunks.tsx
@@ -1,14 +1,15 @@
 import request from '../../../utils/request'
 import { useState, useRef } from 'react'
+import { Scheduler } from '../utils/schduler'
 
 export const useUpLoadChunks = () => {
   const [progress, setProgress] = useState(0)
-
+  const scheduler = new Scheduler(5)
   const upLoadChunks = (fileChunkList, upLoadedChunks = [], file, hash, requestListRef, size) => {
     let count = 0
     const fileSum = fileChunkList.length - upLoadedChunks.length
     const upLoadedChunksSet = new Set(upLoadedChunks.map((item) => Number(item.split('-')[1])))
-    fileChunkList
+    const requireList = fileChunkList
       .map((chunk, index) => {
         const formData = new FormData()
         formData.append('chunk', chunk)
@@ -18,33 +19,34 @@ export const useUpLoadChunks = () => {
         return { formData, index }
       })
       .filter((_, index) => !upLoadedChunksSet.has(index))
-      .map(({ formData, index }) =>
-        request({
-          url: 'http://localhost:3008/my/upload',
-          data: formData,
-          requestListRef,
-        })
-      )
-      .forEach((item) => {
-        item.then((value) => {
-          count++
-          console.log(count, fileChunkList)
-          setProgress((count * 100) / fileSum)
-          if (count === fileSum) {
+      .map(
+        ({ formData, index }) =>
+          () =>
             request({
-              url: 'http://localhost:3008/my/merge',
-              headers: {
-                'content-type': 'application/json',
-              },
-              data: JSON.stringify({
-                fileName: file.name,
-                fileHash: hash,
-                size,
-              }),
+              url: 'http://localhost:3008/my/upload',
+              data: formData,
+              requestListRef,
+            }).then((value) => {
+              count++
+              setProgress((count * 100) / fileSum)
+              if (count === fileSum) {
+                request({
+                  url: 'http://localhost:3008/my/merge',
+                  headers: {
+                    'content-type': 'application/json',
+                  },
+                  data: JSON.stringify({
+                    fileName: file.name,
+                    fileHash: hash,
+                    size,
+                  }),
+                })
+              }
             })
-          }
-        })
-      })
+      )
+    requireList.forEach((item) => {
+      scheduler.add(item)
+    })
   }
   return { progress, upLoadChunks }
 }
