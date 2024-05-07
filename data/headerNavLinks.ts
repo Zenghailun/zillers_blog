b3c018c23bf1a314b5da689795241f33e344dba2
Zeng Hailun <zengboheng01@gmail.com>
2024-05-07 10:57:37 +0800

    feat: update blog content

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,9 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
+  // { href: '/blog', title: 'Blog' },
+  // { href: '/tags', title: 'Tags' },
   { href: '/cloudPhotoAlbum', title: '云相册' },
-  { href: '/about', title: 'About' },
+  // { href: '/about', title: 'About' },
 ]
 
 export default headerNavLinks
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Zillers',
+  author: 'Hailun Zeng',
+  headerTitle: 'Zillers Blog',
+  description: '曾海伦的个人博客',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
@@ -12,7 +12,7 @@ const siteMetadata = {
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'address@yoursite.com',
-  github: 'https://github.com',
+  github: 'https://github.com/Zenghailun',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
